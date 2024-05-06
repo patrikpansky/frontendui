24a8da92c362c2f3d9d77ded2a1507d6e786e5fd
Hrbolek <alexandr.stefek@unob.cz>
2024-05-06 06:37:08 +0200

    ProxyLink with autoreload

--- src/Components/ProxyLink.jsx
+++ src/Components/ProxyLink.jsx
@@ -2,11 +2,16 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom"
 
 export const ProxyLink = ({to, children, ...others}) => {
     const {pathname} = useResolvedPath(to)
-    const item = useMatch(pathname)
+    const base = pathname.split('/')[1]
+    // const item = useMatch(pathname)
+
+    const isLocal = window.location.pathname.startsWith( "/" + base)
+
     // const item2 = useMatch(to)
     // console.log(to, item2)
     // console.log(pathname, item, item2)
-    if (item) {
+    // console.log(to, pathname, isLocal)
+    if (isLocal) {
         return <Link to={to} {...others}>{children}</Link>
     } else {
         return <Link to={to} {...others} reloadDocument={true}>{children}</Link>
