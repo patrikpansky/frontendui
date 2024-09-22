a24bc296e736333f6903cb925f5e763c0c857005
Hrbolek <alexandr.stefek@unob.cz>
2024-09-22 21:33:39 +0200

    the most evolved

--- tools/templates/Components/Link.jsx
+++ tools/templates/Components/Link.jsx
@@ -1,6 +1,6 @@
 import { ProxyLink } from "@hrbolek/uoisfrontend-shared/src";
 
-let linkBase = "/auto/{{name name}}/view/";
+let linkBase = "/all/{{name name}}/view/";
 export const setLinkBase = (value) => linkBase = value;
 export const {{Name name}}Link = ({ {{name name}}, children}) => {
     // console.log("{{Name name}}LargeCard", {{name name}})
