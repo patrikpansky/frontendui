e275ebe9460d294edd348a84d876c23a0ee53bec
Hrbolek <alexandr.stefek@unob.cz>
2024-12-05 19:28:27 +0100

    authorizedFetch2 refactored

--- src/Queries/authorizedFetch.js
+++ src/Queries/authorizedFetch.js
@@ -1,59 +1,54 @@
-const overridenPath = '/api/gql'
-
-const globalFetchParams = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    redirect: 'follow', // manual, *follow, error
-}
-
-// const cache = {}
-const replaceUUID = false
-const replaceID = true
 /**
  * Zapouzdrujici funkce pro fetch, vytvari mezi vrstvu pro komunikace ze serverem
  * @param {*} path 
  * @param {*} params 
  * @returns Promise
  */
-export const authorizedFetch2 = (path, params) => {
+export const authorizedFetch2 = (path, params = {}, options = {}) => {
     // console.log("fetch from shared")
+    const {
+        replaceUUID = false,
+        replaceID = false,
+        globalFetchParams = { 
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            redirect: 'follow', // manual, *follow, error
+        
+        },
+        overridenPath = path, // Default to `path` if not overridden
+    } = options;
 
-    const headers = {...globalFetchParams.headers, ...params.headers}
-    const newParams = {...globalFetchParams, ...params, headers} // allow owerwrite default parameters (globalFetchParams)
-    if (replaceUUID) {
-        newParams.body = newParams.body.replaceAll("UUID", "ID")
-    }
-    if (replaceID) {
-        newParams.body = newParams.body.replaceAll(": ID", ": UUID")
+    // Merge headers and parameters
+    const headers = { ...globalFetchParams.headers, ...params.headers };
+    const newParams = { ...globalFetchParams, ...params, headers };
+
+    // Safely handle body replacements if `body` is a string
+    if (typeof newParams.body === "string") {
+        if (replaceUUID) {
+            newParams.body = newParams.body.replaceAll("UUID", "ID");
+        }
+        if (replaceID) {
+            newParams.body = newParams.body.replaceAll(": ID", ": UUID");
+        }
     }
 
     return fetch(overridenPath, newParams).then(response => {
-        if (response.status === 302) {
-            if (window) {
-                const host = window.location.host
-                const protocol = window.location.protocol
-                const location = response.headers.location
-                const redirectLocation = `${protocol}://${host}${location}`
-                window.location.assign(redirectLocation)
+        if (response.status === 302 && typeof window !== "undefined") {
+            const location = response.headers.get("location");
+            if (location) {
+                const redirectLocation = new URL(location, window.location.origin);
+                window.location.assign(redirectLocation.toString());
             }
         }
-        return response.json()
-    }) //params.header should be extended with Authorization TOKEN
-    // const cached = cache[bodyIndex]
-    // if (cached) {
-    //     console.log("fetch2.cache used")
-    //     return cached
-    // } else {
-    //     const result = fetch(overridenPath, newParams).then(response => response.json()) //params.header should be extended with Authorization TOKEN
-    //     // cache[bodyIndex] = result   
-    //     result.then(
-    //         r => {
-    //             // delete cache[bodyIndex]
-    //         }
-    //     )
-    //     return result
-    // }
+        // Handle non-2xx statuses
+        if (!response.ok) {
+            return Promise.reject(new Error(`HTTP error: ${response.status}`));
+        }
+
+        // Parse and return JSON response
+        return response.json();
+    }) 
 }
